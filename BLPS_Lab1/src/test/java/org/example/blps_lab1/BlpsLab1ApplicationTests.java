f866a8467744e246eed810951f6801dc9f7e751c
jaba <368601@edu.itmo.ru>
2025-02-24 12:22:01 +0300

    add compose and change pom a bit

--- BLPS_Lab1/src/test/java/org/example/blps_lab1/BlpsLab1ApplicationTests.java
+++ BLPS_Lab1/src/test/java/org/example/blps_lab1/BlpsLab1ApplicationTests.java
@@ -3,11 +3,11 @@ package org.example.blps_lab1;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class BlpsLab1ApplicationTests {
+// @SpringBootTest
+// class BlpsLab1ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
