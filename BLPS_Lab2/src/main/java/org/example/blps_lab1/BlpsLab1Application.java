47dd1c6969dac6113746fc0645f44249ba72d0a5
Mikhail Niagin <mikhail.niagin@tune-it.ru>
2025-04-11 03:14:47 +0300

    try narayana

--- BLPS_Lab2/src/main/java/org/example/blps_lab1/BlpsLab1Application.java
+++ BLPS_Lab2/src/main/java/org/example/blps_lab1/BlpsLab1Application.java
@@ -2,12 +2,16 @@ package org.example.blps_lab1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-
+@EnableScheduling
+@EnableTransactionManagement
 @SpringBootApplication
 public class BlpsLab1Application {
 
 	public static void main(String[] args) {
+		System.setProperty("java.naming.factory.initial", "org.jboss.narayana.jta.boot.naming.NarayanaInitialContextFactory");
 		SpringApplication.run(BlpsLab1Application.class, args);
 	}
 
---
+++ BLPS_Lab2/src/main/resources/schema.sql
@@ -0,0 +1,2 @@
+drop table UNI if exists;
+create table UNI(NAME varchar(20) NOT NULL UNIQUE);
