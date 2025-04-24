8527e713e1fc16f6024dba1b7162e0879aef37dd
Mikhail Niagin <mikhail.niagin@tune-it.ru>
2025-04-24 18:21:46 +0300

    all method of admin panel service is now program transaction

--- BLPS_Lab2/src/main/java/org/example/blps_lab1/admin/service/AdminPanelService.java
+++ BLPS_Lab2/src/main/java/org/example/blps_lab1/admin/service/AdminPanelService.java
@@ -3,38 +3,55 @@ package org.example.blps_lab1.admin.service;
 import java.util.Objects;
 
 import org.example.blps_lab1.authorization.models.Role;
+import org.example.blps_lab1.authorization.models.User;
 import org.example.blps_lab1.authorization.service.UserService;
 
+import org.hibernate.Transaction;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
 @Service
 @Slf4j
-@AllArgsConstructor
 public class AdminPanelService {
 
-    private UserService userService;
+    private final UserService userService;
+    private final TransactionTemplate transactionTemplate;
 
-    public void updateRole(String userEmail, String role) {
-        if(Objects.isNull(role) || Objects.isNull(userEmail)){
-            throw new IllegalArgumentException("Поля email и role обязательны");
-        }
-        Role roleToSet;
-        try {
-            roleToSet = Role.valueOf(role);
-        } catch (IllegalArgumentException e) {
-            log.error("Invalid argument, got: {}", role, e);
-            throw new IllegalArgumentException("Неверно указана роль");
-        }
-
-
-        var user = userService.getUserByEmail(userEmail);
-        if (Objects.nonNull(user)) {
-            user.setRole(roleToSet);
-            userService.updateUser(user);
-        }
+    @Autowired
+    public AdminPanelService(UserService userService, PlatformTransactionManager platformTransactionManager) {
+        this.userService = userService;
+        this.transactionTemplate = new TransactionTemplate(platformTransactionManager);
     }
 
+    public void updateRole(String userEmail, String role) {
+        transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+            @Override
+            protected void doInTransactionWithoutResult(@NotNull TransactionStatus status) {
+                if (Objects.isNull(role) || Objects.isNull(userEmail)) {
+                    throw new IllegalArgumentException("Поля email и role обязательны");
+                }
+                Role roleToSet;
+                try {
+                    roleToSet = Role.valueOf(role);
+                } catch (IllegalArgumentException e) {
+                    log.error("Invalid argument, got: {}", role, e);
+                    throw new IllegalArgumentException("Неверно указана роль");
+                }
+
+                var user = userService.getUserByEmail(userEmail);
+                if (Objects.nonNull(user)) {
+                    user.setRole(roleToSet);
+                    userService.updateUser(user);
+                }
+            }
+        });
+    }
 }
